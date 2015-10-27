70bc08f17843dbac5bdb1065193ca6555bf7bdbf
Kasper Emil Okkels <kasperemil38@gmail.com>
2015-10-27 21:52:45 +0100

    Oprettet midlertidige metoder til at ændre i point stillingen.

--- src/cdio_2/Bank.java
+++ src/cdio_2/Bank.java
@@ -8,4 +8,12 @@ private int balance;
 public Bank(){
 	balance=1000;
 		}
+public int gain(){
+	balance=balance+amount;
+	return balance;
+}
+public int lose(){
+	balance=balance-amount;
+	return balance;
+}
 }
