8c132f58f5ee9ba9ba16ec7d2da0db5c7199c089
Kasper Emil Okkels <kasperemil38@gmail.com>
2015-10-28 21:01:39 +0100

    Printer sat op til at printe, dog uden tekster til felterne.

--- src/cdio_2/Printer.java
+++ src/cdio_2/Printer.java
@@ -1,6 +1,43 @@
 package cdio_2;
 
 public class Printer {
-
+public void print(){
+	Dice dice= new Dice();
+//println funktionerne skal udfyldes med tilhørene felt tekst
+switch(dice.getsum()){
+case 2:
+	System.out.println();
+	break;
+case 3:
+	System.out.println();
+	break;
+case 4:
+	System.out.println();
+	break;
+case 5:
+	System.out.println();
+	break;
+case 6:
+	System.out.println();
+	break;
+case 7:
+	System.out.println();
+	break;
+case 8:
+	System.out.println();
+	break;
+case 9:
+	System.out.println();
+	break;
+case 10:
+	System.out.println();
+	break;
+case 11:
+	System.out.println();
+	break;
+case 12:
+	System.out.println();
+	break;
+}
 }
 
