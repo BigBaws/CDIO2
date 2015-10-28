11b27cf10a3a96bb7fc801595d9a64ee761a6e73
Kasper Emil Okkels <kasperemil38@gmail.com>
2015-10-28 19:55:54 +0100

    Skrevet rolldice funktion.

--- src/cdio_2/Bank.java
+++ src/cdio_2/Bank.java
@@ -8,6 +8,7 @@ private int balance;
 public Bank(){
 	balance=1000;
 		}
+//amount skal erstates med et kald der høre til det enkelte felts point uddeling (metode kald).
 public int gain(){
 	balance=balance+amount;
 	return balance;
--- src/cdio_2/Dice.java
+++ src/cdio_2/Dice.java
@@ -1,5 +1,13 @@
 package cdio_2;
 
 public class Dice {
+	private int die1;
+	private int die2;
+	
+	public int rolldice(){
+		die1=(int) ((Math.random()*6)+1);
+		die2=(int) ((Math.random()*6)+1);
+		return die1+die2;
+	}
 	
 }
