f0654ddcc87a51a837bd4464c501f73979e76f01
Kasper Emil Okkels <kasperemil38@gmail.com>
2015-10-30 09:39:29 +0100

    Udfyldt tekst til printer funktionen

--- src/cdio_2/Printer.java
+++ src/cdio_2/Printer.java
@@ -6,37 +6,48 @@ public void print(){
 //println funktionerne skal udfyldes med tilhørene felt tekst
 switch(dice.getsum()){
 case 2:
-	System.out.println();
+	//tower
+	System.out.println("With the help of the alchemists of the tower, you invent a wonder drug that cures any cold instantaniuosly. Financial succes Ho', you gain " +amount);
 	break;
 case 3:
-	System.out.println();
+	//crater
+	System.out.println("Your caravan suddenly turned into a crater. WHAT HAPPENED!?, you lose " +amount);
 	break;
 case 4:
-	System.out.println();
+	//palace gates
+	System.out.println("You meet many nobles at the palace gates and manage to convince some of them to fund a caravan expedition, you gain " +amount);
 	break;
 case 5:
-	System.out.println();
+	//cold desert
+	System.out.println("The Desert is much colder than you would've ever imagined, you light a fire using some of your wares, you lose "+amount);
 	break;
 case 6:
-	System.out.println();
+	//walled city
+	System.out.println("The Walled City seldomly lets in outsiders, your wares are foreign and exotic to it's inhabitants. Your wares sells incredibly well, you gain "+amount);
 	break;
 case 7:
-	System.out.println();
+	//monastary +0
+	System.out.println("The monastary provides shelter from all the evil in the world but there isn't any economical gain in visiting it.");
 	break;
 case 8:
-	System.out.println();
+	//black cave -70
+	System.out.println("Pirates from the black cave rob you, you lose "+amount);
 	break;
 case 9:
-	System.out.println();
+	// huts in the mountains +60
+	System.out.println("The harsh people of the mountains are skilled artisans but lack merchant experience. You manage to stuck up your wares cheaper than you would've in a city, you gain " +amount);
 	break;
 case 10:
-	System.out.println();
+	//the werewall (werewolf-wall) -80 + ekstra tur
+	System.out.println("Meeting the gang of werewolf that always walks in a row, known as 'The Werewall', isn't pleasant but it gets your adrenaline pumping when you suddenly need to run for your life, you lose "+amount +"but you get an additional turn");
 	break;
 case 11:
-	System.out.println();
+	//the pit -50
+	System.out.println("No man can go without exitement you spend 50 as entrance fee into the lounge area of the PIT, the match was exciting but no money is earned, you lose " +amount);
 	break;
 case 12:
-	System.out.println();
+	//goldmine +650
+	System.out.println("The mine you own has struck gold, you gain" + amount +"Hussah");
 	break;
 }
 }
