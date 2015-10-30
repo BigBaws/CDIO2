32bcc3490105da280b4e4b5ac0883104c6d55615
Kasper Emil Okkels <kasperemil38@gmail.com>
2015-10-30 07:41:45 +0100

    Rettet fejl i metode createplayer metoden

--- src/cdio_2/Player.java
+++ src/cdio_2/Player.java
@@ -3,7 +3,7 @@ package cdio_2;
 public class Player {
 private String name;
 	public void createplayer(){
-		String name="input";
+		name="input";
 	}
 	public String getplayername(){
 		return name;
