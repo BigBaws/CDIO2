9654a4acd5d484bfc1d4d7736a6c56cdd6bf43fd
Kasper Emil Okkels <kasperemil38@gmail.com>
2015-10-31 22:24:25 +0100

    Lavet Turn klasse til at styre turens forløb

---
+++ src/cdio_2/Turn.java
@@ -0,0 +1,47 @@
+package cdio_2;
+
+public class Turn {
+Dice dice = new Dice();
+private int points;
+public int round(){
+	dice.rolldice();
+	switch (dice.getsum()){
+	case 2:
+		points= 250;
+	break;
+	case 3:
+		points= -100;
+		break;
+	case 4:
+		points= 100;
+		break;
+	case 5:
+		points= -20;
+		break;
+	case 6:
+		points= 180;
+		break;
+	case 7:
+		points= 0;
+		break;
+	case 8:
+		points= -70;
+		break;
+	case 9:
+		points= 60;
+		break;
+	case 10:
+		points=-80+round();
+		break;
+	case 11:
+		points= -50;
+		break;
+	case 12:
+		points= 650;
+		break;
+	}
+	return points;
+	
+}
+	
+}
